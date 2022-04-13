1e87021afd699a9ec3195a7b555a4ba5edeb6db5
Mukul Kadyan <mukulkadyan202@gmail.com>
2022-04-13 17:53:21 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,8 +29,8 @@ export default function Home({ posts }: Props) {
   <div className="text-xl  p-2  md:text-1.5xl">Student run newsletter of IIT(ISM) Dhanbad.</div>
 </header>
         <img
-          className="hidden h-32 md:inline-flex lg:h-full bg-inherit"
-          src="https://cdn.iconscout.com/icon/premium/png-256-thumb/medium-3324353-2768644.png"
+          className="hidden h-32 md:inline-flex lg:h-32 bg-inherit"
+          src="https://image.winudf.com/v2/image1/Y29tLm1haWxlcmRhZW1vbi5hcHBfc2NyZWVuXzBfMTU4MDE4MzI5NV8wOTI/screen-0.jpg?fakeurl=1&type=.jpg"
           alt=""
         />
       </div>
