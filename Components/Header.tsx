25a044083d27dae3c2776a5db94b70ab2b85136d
Mukul Kadyan <mukulkadyan202@gmail.com>
2022-04-13 18:05:04 +0530

    Update Header.tsx

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -24,11 +24,11 @@ function Header() {
       </div>
       <div className="flex flex-row ">
         <a href='https://placementor-2021.herokuapp.com/' target={'_blank'}>
-        <button type="button" className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Placementor</button>
+        <button type="button" className="text-gray-900 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Placementor</button>
       </a>
       {/* </Link> */}
       <a href='https://www.iitism.ac.in/iitismnew/' target='_blank'>
-      <button type="button" className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">IIT-ISM</button>
+      <button type="button" className="text-gray-900 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">IIT-ISM</button>
       </a>
       </div>
     </header>
