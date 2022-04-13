7f2a22648ae8652a5bbf9d0a0d89601b125ca8b7
Mukul Kadyan <mukulkadyan202@gmail.com>
2022-04-13 18:16:18 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home({ posts }: Props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div className="flex items-center justify-between border-y border-black bg-gradient-to-r from-green-400 to-blue-500 py-10 lg:py-0 mt-4 mb-5 text-white-400 hover:text-sky-400">
+      <div className="flex items-center justify-center border-y border-black bg-gradient-to-r from-green-400 to-blue-500 py-10 lg:py-0 mt-4 mb-5 text-white-400">
    
      <header className="mb-16 group">
   <h1 className="max-w-xl font-serif text-4xl md:text-6xl">
@@ -28,11 +28,11 @@ export default function Home({ posts }: Props) {
   </h1>
   <div className="text-xl  p-2  md:text-1.5xl">Student run newsletter of IIT(ISM) Dhanbad.</div>
 </header>
-        <img
+<!--         <img
           className="hidden h-32 md:inline-flex lg:h-32 bg-inherit"
           src="https://image.winudf.com/v2/image1/Y29tLm1haWxlcmRhZW1vbi5hcHBfc2NyZWVuXzBfMTU4MDE4MzI5NV8wOTI/screen-0.jpg?fakeurl=1&type=.jpg"
           alt=""
-        />
+        /> -->
       </div>
       {/* posts */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
